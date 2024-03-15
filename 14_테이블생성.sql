83f77efee6687ac39375599405b90a3cc0f64bcc
jg <iamjgl@naver.com>
2024-03-15 21:41:06 +0900

    sql pl

--- 14_테이블생성.sql
+++ 14_테이블생성.sql
@@ -30,3 +30,48 @@ VALUES(20, '개발부', '서울', sysdate, 2000000);
 INSERT INTO dept2
 VALUES(20, '개발부워ㅜㄹ넝런어룬어ㅜㄹㄴㅇㄹㄴㅇㄹㄴㅇㄹㄴㅇ', '서ㄴㄹㄴㅇㄹㄴㅇㄹ울', sysdate, 200000000000000); --세팅한 글자수 초과
 
+
+-- 컬럼 추가(항목)
+ALTER TABLE dept2
+ADD dept_count NUMBER(3); --기본값 null
+
+--컬럼명 변경
+ALTER TABLE dept2
+RENAME COLUMN dept_count TO emp_count;
+SELECT * FROM dept2;
+
+--컬럼 속성 수정
+--만약 변경하고자 하는 컬럼에 데이터가 이미 존재한다면, 그에 맞는 타입으로 변경해 주셔야 합니다, 맞지 않는 타입으로는 변경 불가(값이 null일 경우 ?外).
+ALTER TABLE dept2
+MODIFY dept_name VARCHAR2(50); --VARCHAR2(15)->VARCHAR2(50) 길이변경
+DESC dept2; --길이 확인
+
+
+
+ALTER TABLE dept2
+MODIFY dept_name NUMBER(50); --이미 VARCHAR 타입 있어서 NUMBER 타입으로 변경 불가
+
+ALTER TABLE dept2
+MODIFY emp_count VARCHAR2(20); --테이블에 데이터 null이라서 속성 변경 가능 ***커밋과 롤백에 영향 XXX
+--DDL 자동 커밋됨 : 롤백(복구) 안됨 : INSERT UPDATE DELETE만 ROLLBACK 가능
+
+--DDL(CREATE, ALTER, TRUNCATE, DROP)은 트랜잭션(ROLLBACK)의 대상이 아닙니다.
+ROLLBACK;
+
+--컬럼 삭제
+ALTER TABLE dept2
+DROP COLUMN dept_count; --!주의 : 내부에 데이터 있어도 삭제됨
+
+--테이블 이름 변경
+ALTER TABLE dept2
+RENAME TO dept3;
+
+--테이블 삭제(구조는 남겨두고 내부 데이터만 모두 삭제)--함부러 XXX : 사본사용 or merge사용
+TRUNCATE TABLE dept3;
+
+--테이블 삭제
+DROP TABLE dept2; --아예 삭제(ROLLBACK; 不可)--함부러 XXX
+
+--DDL(CREATE, ALTER, TRUNCATE, DROP) != 트랜잭션 : 복구 안됨
+--DML(SELECT, INSERT, UPDATE, DELETE)
+--TCL(COMMIT, ROLLBACK, SAVEPOINT)
